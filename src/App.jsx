6998aae7b6b9acc9b0a21daaeca54d0676aeeb5a
kuntowck <kunto312002@gmail.com>
2024-08-20 20:55:36 +0700

    create a new item on the component form

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const groceryItems = [
   {
     id: 1,
@@ -35,18 +37,43 @@ function Header() {
 }
 
 function Form() {
+  const [name, setName] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!name) return;
+
+    const newItem = { name, quantity, checked: false, id: Date.now() };
+    console.log(newItem);
+
+    setName("");
+    setQuantity(1);
+  }
+
+  const quantityNum = [...Array(10)].map((_, i) => (
+    <option value={i + 1} key={i + 1}>
+      {i + 1}
+    </option>
+  ));
+
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>Hari ini belanja apa kita?</h3>
       <div>
-        <select>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
+        <select
+          value={quantity}
+          onChange={(e) => setQuantity(Number(e.target.value))}
+        >
+          {quantityNum}
         </select>
-        <input type="text" placeholder="nama barang..." />
+        <input
+          type="text"
+          placeholder="nama barang..."
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
       </div>
       <button>Tambah</button>
     </form>
@@ -59,7 +86,7 @@ function GroceryList() {
       <div className="list">
         <ul>
           {groceryItems.map((item) => (
-            <Item item={item} key={item.id}/>
+            <Item item={item} key={item.id} />
           ))}
         </ul>
       </div>
@@ -75,7 +102,7 @@ function GroceryList() {
   );
 }
 
-function Item({item}) {
+function Item({ item }) {
   return (
     <li key={item.id}>
       <input type="checkbox" />
